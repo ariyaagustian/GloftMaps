d1d42f7b6f491fe1ed747b2479ef97ddbe7122ca
Indra Gunanda <indra.gunanda@gmail.com>
2018-08-23 14:27:11 +0700

    Ref

--- application/controllers/admin/Rute.php
+++ application/controllers/admin/Rute.php
@@ -50,58 +50,80 @@ class Rute extends CI_Controller
       $data[] = (object)["lat"=>$value->latitude,"lng"=>$value->longitude];
     }
     $temp = [];
+    $min = [];
+    $tes = null;
     foreach ($data as $key1 => $value1) {
       $origin = $value1;
       foreach ($data as $key2 => $value2) {
         if($key2 > $key1){
-          $temp[$key1][] = ["origin"=>$origin->lat.",".$origin->lng,"dest"=>$value2->lat.",".$value2->lng];
+          // $distance = ( 6371 * acos((cos(deg2rad($origin->lat) ) * (cos(deg2rad($value2->lat))) * (cos(deg2rad($value2->lng) - deg2rad($origin->lng)) )+ ((sin(deg2rad($origin->lat)) * (sin(deg2rad($value2->lat))))))));
+            $distance = ( 6371 * acos((cos(deg2rad($origin->lat)) ) * (cos(deg2rad($value2->lat))) * (cos(deg2rad($value2->lng) - deg2rad($origin->lng)) )+ ((sin(deg2rad($origin->lat))) * (sin(deg2rad($value2->lat))))) );
+          if ($tes == null) {
+            $tes = $distance;
+          }else {
+            if ($tes > $distance) {
+              $tes = $distance;
+            }
+          }
+          $temp[$key1][] = ["origin"=>$origin->lat.",".$origin->lng,"dest"=>$value2->lat.",".$value2->lng,"jarak"=>$distance];
         }
       }
+      $min[] = $tes;
+      // usort($temp[$key1], function($a, $b) {
+      //   return $a['jarak'] <=> $b['jarak'];
+      // });
     }
     foreach ($temp as $k => &$v) {
       foreach ($v as $k2 => &$v2) {
           $distance = ( 6371 * acos((cos(deg2rad(explode(",",$v2["origin"])[0])) ) * (cos(deg2rad(explode(",",$v2["dest"])[0]))) * (cos(deg2rad(explode(",",$v2["dest"])[1]) - deg2rad(explode(",",$v2["origin"])[1])) )+ ((sin(deg2rad(explode(",",$v2["origin"])[0]))) * (sin(deg2rad(explode(",",$v2["dest"])[0]))))) );
           $v2["jarak"] = $distance;
       }
+      usort($v, function($a, $b) {
+        return $a['jarak'] <=> $b['jarak'];
+      });
     }
     // $temp = $temp[0];
-    foreach ($temp as $key => &$value) {
-      foreach ($value as $ey => &$alue) {
-        unset($temp[$key][$ey]["origin"]);
-        unset($temp[$key][$ey]["dest"]);
-      }
-
-    }
-    $num = function($no = 1,$string="I"){
-      $temp = [];
-      for ($n=0; $n < $no ; $n++) {
-        $temp[$n] = $string;
-      }
-      return implode("",$temp);
-    };
-    $as = [];
-    foreach ($temp as $key => $value) {
-      $a = [];
-      foreach ($value as $ky => $vlue) {
-        $a[$num(($ky+1),"O")] = $vlue["jarak"];
-      }
-      $as[$num(($key+1),"I")] = $a;
-    }
-    // $temp = [$temp];
-    // $temp = array(
-    //   'A' => array('B' => 9, 'D' => 14, 'F' => 7),
-    //   'B' => array('A' => 9, 'C' => 11, 'D' => 2, 'F' => 10),
-    //   'C' => array('B' => 11, 'E' => 6, 'F' => 15),
-    //   'D' => array('A' => 14, 'B' => 2, 'E' => 9),
-    //   'E' => array('C' => 6, 'D' => 9),
-    //   'F' => array('A' => 7, 'B' => 10, 'C' => 15),
-    //   'G' => array()
-    // );
-    $algorithm = new \Fisharebest\Algorithm\Dijkstra($as);
-    $path = $algorithm->shortestPaths("III", "II");
+    // foreach ($temp as $key => &$value) {
+    //   foreach ($value as $ey => &$alue) {
+    //     unset($temp[$key][$ey]["origin"]);
+    //     unset($temp[$key][$ey]["dest"]);
+    //   }
+    //
+    // }
+    // $num = function($no = 1,$string="I"){
+    //   $temp = [];
+    //   for ($n=0; $n < $no ; $n++) {
+    //     $temp[$n] = $string;
+    //   }
+    //   return implode("",$temp);
+    // };
+    // $as = [];
+    // foreach ($temp as $key => $value) {
+    //   $a = [];
+    //   foreach ($value as $ky => $vlue) {
+    //     $a[$num(($ky+1),"O")] = $vlue["jarak"];
+    //   }
+    //   $as[$num(($key+1),"I")] = $a;
+    // }
+    // $ax = $temp[0];
+    // usort($ax, function($a, $b) {
+    //   return $a['jarak'] <=> $b['jarak'];
+    // });
+    // $temp = [$ax];
+    // // $temp = array(
+    // //   'A' => array('B' => 9, 'D' => 14, 'F' => 7),
+    // //   'B' => array('A' => 9, 'C' => 11, 'D' => 2, 'F' => 10),
+    // //   'C' => array('B' => 11, 'E' => 6, 'F' => 15),
+    // //   'D' => array('A' => 14, 'B' => 2, 'E' => 9),
+    // //   'E' => array('C' => 6, 'D' => 9),
+    // //   'F' => array('A' => 7, 'B' => 10, 'C' => 15),
+    // //   'G' => array()
+    // // );
+    // $algorithm = new \Fisharebest\Algorithm\Dijkstra($as);
+    // $path = $algorithm->shortestPaths("III", "II");
 
     header('Content-Type: application/json');
-    echo json_encode($path);
+    echo json_encode($min);
   }
   private function jarak($origin,$dest)
   {
